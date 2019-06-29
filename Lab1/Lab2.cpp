7b11b424ce541ce1c271a2a72711d7fde5151135
Supagin25130 <52159977+Supagin25130@users.noreply.github.com>
2019-06-29 14:07:43 +0700

    Homework

---
+++ Lab1/Lab2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main()
+{
+ string Name = "Supagin Chengkhao";
+ short Scores1 = 99;
+ short Scores2 = 80;
+ short Total = 179;
+ char grade = 'B';
+ float gpa = 4.00;
+ cout << "My name is :" << Name << endl;
+ cout << "My scores 1 :" << Scores1 << endl;
+ cout << "My scores 2 :" << Scores2 << endl;
+ cout << "Total :" << Total << endl;
+ cout << "Grade :" << grade << endl;
+ cout << "GPA :" << gpa << endl;
+}
